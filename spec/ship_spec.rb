a2b65504fd0dfb5dd0afd7b3c46bf99530471963
Nick Cooley <cooley8706@gmail.com>
2021-12-09 17:26:02 -0700

    test: Pass length test with initialize method

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Ship do
     expect(cruiser).to be_instance_of(Ship)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.name).to eq("Cruiser")
   end
 
-  xit 'has a length' do
+  it 'has a length' do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser.length).to eq(3)
   end
