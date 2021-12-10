b5703b304066245fff0b5506ea353dcfdfb75f56
Nick Cooley <cooley8706@gmail.com>
2021-12-09 19:12:01 -0700

    feat: All methods defined

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,17 @@
-#git push origin ship
 class Ship
   attr_reader :health, :name, :length
   def initialize(name, length)
   @name = name
   @health = length
   @length = length
+  end
+
+  def sunk?
+    @health < 1
+  end
+
+  def hit
+    @health -= 1
   end
 
 end
