9bd16fdccbcd8e045f269e0ad36e85abf33ce8a9
Jesse Ochs <jessescottochs@gmail.com>
2021-12-12 09:50:57 -0700

    Test: all tests pass for valid placement method in placement class

--- spec/placement_spec.rb
+++ spec/placement_spec.rb
@@ -9,82 +9,82 @@ RSpec.describe Placement do
   expect(placement).to be_instance_of(Placement)
   end
 
-  it 'places Cruiser correctly ' do
+  it 'places Cruiser correctly' do
     cruiser = Ship.new("Cruiser", 3)
     coordinates = ["A1", "A2", "A3"]
     placement = Placement.new(cruiser, coordinates)
     expect(placement.valid_placement?).to be true
   end
 
- it 'places Cruiser incorrectly ' do
+ it 'coordinate size does not match ship length' do
    cruiser = Ship.new("Cruiser", 3)
    coordinates = ["A1", "A2"]
    placement = Placement.new(cruiser, coordinates)
    expect(placement.valid_placement?).to be false
  end
 
- it 'places Submarine incorrectly ' do
+ it 'coordinate size does not match ship length' do
    submarine = Ship.new("Submarine", 2)
    coordinates = ["A2", "A3", "A4"]
    placement = Placement.new(submarine, coordinates)
    expect(placement.valid_placement?).to be false
  end
 
- it 'places Cruiser consecutively' do
-   cruiser = Ship.new("Cruiser", 4)
-   coordinates = ["A4", "A3", "A2", "A1"]
+ it 'coordinates are not consecutive' do
+   cruiser = Ship.new("Cruiser", 3)
+   coordinates = ["A1", "A2", "A4"]
    placement = Placement.new(cruiser, coordinates)
-
    expect(placement.valid_placement?).to eq false
  end
 
- it 'places Cruiser consecutively backwards' do
+ it 'coodinates are not consecutive' do
+   submarine = Ship.new("Submarine", 2)
+   coordinates = ["A1", "C1"]
+   placement = Placement.new(submarine, coordinates)
+   expect(placement.valid_placement?).to be false
+ end
+
+ it 'coodrinates can not be verticle' do
    cruiser = Ship.new("Cruiser", 3)
    coordinates = ["A3", "A2", "A1"]
    placement = Placement.new(cruiser, coordinates)
    expect(placement.valid_placement?).to eq false
  end
 
- xit 'places Submarine consecutively backwards' do
+ it 'coordinates can not be verticle' do
    submarine = Ship.new("Submarine", 2)
    coordinates = ["C1", "B1"]
    placement = Placement.new(submarine, coordinates)
-   placement.split_keys
    expect(placement.valid_placement?).to be false
  end
 
- xit 'places Cruiser diagonally' do
+ it 'coordinates can not be diagonal' do
    cruiser = Ship.new("Cruiser", 3)
    coordinates = ["A1", "B2", "C3"]
    placement = Placement.new(cruiser, coordinates)
-   placement.split_keys
    expect(placement.valid_placement?).to eq false
  end
 
- xit 'places Submarine diagonally' do
+ it 'coordinates can not be diagonal' do
    submarine = Ship.new("Submarine", 2)
    coordinates = ["C2", "D3"]
    placement = Placement.new(submarine, coordinates)
-   placement.split_keys
    expect(placement.valid_placement?).to be false
  end
 
- xit 'places Submarine valid' do
+ it 'places Submarine valid' do
    submarine = Ship.new("Submarine", 2)
    coordinates = ["A1", "A2"]
    placement = Placement.new(submarine, coordinates)
-   placement.split_keys
    expect(placement.valid_placement?).to be true
  end
 
- xit 'places Cruiser valid' do
+ it 'places Cruiser valid' do
    cruiser = Ship.new("Cruiser", 3)
    coordinates = ["B1", "C1", "D1"]
    placement = Placement.new(cruiser, coordinates)
-   placement.split_keys
    expect(placement.valid_placement?).to eq true
  end
-
 
 
 end
